8aaa27fafb2d460d450b4be01292c705c9d2ee57
George <backs159@icloud.com>
2022-05-24 21:11:33 +0500

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Георгий Кузнецов on 24.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
